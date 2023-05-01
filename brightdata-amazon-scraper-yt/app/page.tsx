5ac2673986c33ae18e5288bb5d9a73205007145e
Vladimir0657305 <vv0657305@gmail.com>
2023-05-01 17:54:51 +0300

    changes

--- brightdata-amazon-scraper-yt/app/page.tsx
+++ brightdata-amazon-scraper-yt/app/page.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { DocumentMagnifyingGlassIcon } from '@heroicons/react/24/outline'
+import { DocumentMagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 
 function HomePage() {
   return (
-      <div>
+    <div className='flex flex-col items-center justify-center'>
+        <div className='flex flex-col items-center justify-center'>
             <div>
-                <DocumentMagnifyingGlassIcon className="h-64 w-64" />
+                <DocumentMagnifyingGlassIcon className="h-64 w-64 text-indigo-600/20 " />
             </div>
+            <h1 className='text-3xl mt-2 text-center text-black font-bold mb-5'>Welcome to the Amazon Web Scraper</h1>
+            <h2 className='text-lg italic text-center text-black/50'>Here you can get all you want</h2>
+            <h3 className='text-lg text-center italic text-black/50'>
+                https://amazon.com
+            </h3>
         </div>
+    </div>
   )
 }
 
---
+++ brightdata-amazon-scraper-yt/components/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <header>
+            <form action="">
+                <input type="text" placeholder="Search" />
+                <button>Search</button>
+            </form>
+        </header>
+    )
+}
+
+export default Header
